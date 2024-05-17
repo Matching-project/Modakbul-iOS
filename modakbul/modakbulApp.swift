556ac873d7096b1013a2345090356dfd7a7b518b
keem-hyun <hw813@naver.com>
2024-05-17 15:46:00 +0900

    setting

---
+++ modakbul/modakbulApp.swift
@@ -0,0 +1,17 @@
+//
+//  modakbulApp.swift
+//  modakbul
+//
+//  Created by 김강현 on 5/17/24.
+//
+
+import SwiftUI
+
+@main
+struct modakbulApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
