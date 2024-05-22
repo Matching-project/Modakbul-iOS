2da69188a908c675c2810840a7f3508b2f6c2f9d
YunSwain <destap@naver.com>
2024-05-22 17:48:35 +0900

    feat: 탭뷰 선언

--- Modakbul/ContentView.swift
+++ Modakbul/ContentView.swift
@@ -7,15 +7,25 @@
 
 import SwiftUI
 
+@frozen enum PageType {
+    case home, chattings, settings
+}
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            HomeView()
+                .tag(PageType.home)
+                .tabItem { Label("홈", systemImage: "house") }
+            
+            Text("채팅 내역")
+                .tag(PageType.chattings)
+                .tabItem { Label("채팅", systemImage: "bubble") }
+            
+            Text("My페이지")
+                .tag(PageType.settings)
+                .tabItem { Label("My", systemImage: "person") }
         }
-        .padding()
     }
 }
 
