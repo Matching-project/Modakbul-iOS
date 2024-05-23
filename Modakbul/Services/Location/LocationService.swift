11d86c7f0ec8312acea5ce19f0418ae492562d0b
YunSwain <destap@naver.com>
2024-05-23 16:51:24 +0900

    feat: 지도 인터페이스 구현

--- Modakbul/Services/Location/LocationService.swift
+++ Modakbul/Services/Location/LocationService.swift
@@ -8,27 +8,64 @@
 import Foundation
 import CoreLocation
 
+protocol LocationServiceDelegate {
+    func didUpdateLocations(locations: [CLLocation])
+    func didFailWithError(error: LocationServiceError)
+}
+
 protocol LocationService: NSObject {
     func start()
     func stop()
+    func connectOnce()
+}
+
+enum LocationServiceError: Error {
+    case notAuthorized
+    case network
+    case headingFailure
+    case locationUnknown
+    case other(code: Int)
+    case unknownError(generic: Error)
 }
 
 final class DefaultLocationService: NSObject {
     private let locationManager: LocationManager
+    var delegate: LocationServiceDelegate?
     
     init(
-        locationManager: LocationManager,
-        delegate: CLLocationManagerDelegate
+        locationManager: LocationManager = CLLocationManager(),
+        delegate: LocationServiceDelegate? = nil
     ) {
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.distanceFilter = kCLDistanceFilterNone
         self.locationManager = locationManager
+        self.delegate = delegate
         super.init()
         locationManager.delegate = self
+    }
+    
+    private func resolveError(_ error: Error) -> LocationServiceError {
+        guard let error = error as? CLError else { return .unknownError(generic: error) }
+        switch error.code {
+        case .denied, .promptDeclined: return .notAuthorized
+        case .headingFailure: return .headingFailure
+        case .network: return .network
+        case .locationUnknown: return .locationUnknown
+        default: return .unknownError(generic: error)
+        }
     }
 }
 
 // MARK: CLLocationManagerDelegate Confirmation
 extension DefaultLocationService: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        delegate?.didUpdateLocations(locations: locations)
+    }
     
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: any Error) {
+        let error = resolveError(error)
+        delegate?.didFailWithError(error: error)
+    }
 }
 
 // MARK: LocationService Confirmation
@@ -40,5 +77,9 @@ extension DefaultLocationService: LocationService {
     
     func stop() {
         locationManager.stopUpdatingLocation()
+    }
+    
+    func connectOnce() {
+        locationManager.requestLocation()
     }
 }
