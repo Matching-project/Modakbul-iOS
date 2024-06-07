621b20719f359d3723b1a58c880f6d9b075ac914
YunSwain <destap@naver.com>
2024-06-07 17:35:17 +0900

    feat: UserDTO 정의

--- Modakbul/Data/DTOs/UserDTO.swift
+++ Modakbul/Data/DTOs/UserDTO.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+struct UserDTO: Decodable {
+    let Id: String
+    let email: String
+    let name: String
+    let birth: String
+    let nickname: String
+    let gender: String
+    let isGenderVisible: Bool
+    let image: String?
+    let categoryName: String
+    
+    func toDTO(provider: AuthenticationProvider) -> User {
+        return User(
+            id: Id,
+            email: email,
+            provider: provider,
+            name: name,
+            gender: Gender(string: gender),
+            isGenderVisible: isGenderVisible,
+            birth: birth,
+            nickname: nickname,
+            imageURL: image,
+            category: Category(string: categoryName)
+        )
+    }
+}
