82b4fc57d3d1aeb690639eec3ac23efa2dcf5877
YunSwain <destap@naver.com>
2024-06-07 17:39:21 +0900

    feat: 토큰 캐싱 목적의 저장소 구현

--- Modakbul/Infrastructure/Storages/TokenStorage/TokenStorage.swift
+++ Modakbul/Infrastructure/Storages/TokenStorage/TokenStorage.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+protocol TokenStorage {
+    func fetchToken(id userId: String) async throws -> (accessToken: String, refreshToken: String)
+    func storeToken(id userId: String, tokens: (accessToken: String, refreshToken: String)) async
+}
+
+fileprivate enum TokenStorageError: Error {
+    case canNotFindToken
+}
+
+final class DefaultTokenStorage {
+    typealias UserID = String
+    typealias Tokens = (accessToken: String, refreshToken: String)
+    
+    private var container: [UserID: Tokens] = [:]
+}
+
+// MARK: TokenStorage Confirmation
+extension DefaultTokenStorage: TokenStorage {
+    func fetchToken(id userId: UserID) async throws -> Tokens {
+        guard let tokens = container[userId] else {
+            throw TokenStorageError.canNotFindToken
+        }
+        return tokens
+    }
+    
+    func storeToken(id userId: UserID, tokens: Tokens) async {
+        container[userId] = tokens
+    }
+}
