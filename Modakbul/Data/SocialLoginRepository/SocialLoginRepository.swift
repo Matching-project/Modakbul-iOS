385dcd4c46551b40632b3cc936dc89a0fef8ba50
YunSwain <destap@naver.com>
2024-06-07 17:35:36 +0900

    feat: 소셜로그인 저장소 구현

--- Modakbul/Data/SocialLoginRepository/SocialLoginRepository.swift
+++ Modakbul/Data/SocialLoginRepository/SocialLoginRepository.swift
@@ -1,5 +1,5 @@
 //
-//  TokenRepository.swift
+//  SocialLoginRepository.swift
 //  Modakbul
 //
 //  Created by Swain Yun on 6/6/24.
@@ -8,6 +8,47 @@
 import Foundation
 
 protocol SocialLoginRepository {
-    func fetchToken(by userId: String) async throws -> String
-    func storeToken(by userId: String, with token: String) async throws
+    func onOpenURL(url: URL)
+    func login(with provider: AuthenticationProvider) async throws -> User
+}
+
+fileprivate enum SocialLoginRepositoryError: Error {
+    case authorizeFailed
+    case authorizeCancelled
+}
+
+final class DefaultSocialLoginRepository {
+    private let tokenStorage: TokenStorage
+    private let authorizationService: AuthorizationService
+    private let networkService: NetworkService
+    
+    init(
+        tokenStorage: TokenStorage,
+        authorizationService: AuthorizationService,
+        networkService: NetworkService
+    ) {
+        self.tokenStorage = tokenStorage
+        self.authorizationService = authorizationService
+        self.networkService = networkService
+    }
+}
+
+// MARK: SocialLoginRepository Confirmation
+extension DefaultSocialLoginRepository: SocialLoginRepository {
+    func onOpenURL(url: URL) {
+        authorizationService.handleOpenURL(url: url)
+    }
+    
+    func login(with provider: AuthenticationProvider) async throws -> User {
+        do {
+            let (accessToken, refreshToken) = try await authorizationService.authorize(with: provider)
+            let endpoint = Endpoint.socialLogin(accessToken: accessToken, refreshToken: refreshToken)
+            let user = try await networkService.request(endpoint: endpoint, for: UserDTO.self).toDTO(provider: provider)
+            await tokenStorage.storeToken(id: user.id, tokens: (accessToken, refreshToken))
+            return user
+        } catch {
+            print("-------------------------\(error)")
+            throw error
+        }
+    }
 }
