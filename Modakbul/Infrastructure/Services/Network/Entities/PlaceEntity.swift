2ff70a82e1b8e52407a84e3ea4680dfdf8fef42f
YunSwain <destap@naver.com>
2024-06-17 02:17:03 +0900

    feat: 장소 엔티티 정의

--- Modakbul/Infrastructure/Services/Network/Entities/PlaceEntity.swift
+++ Modakbul/Infrastructure/Services/Network/Entities/PlaceEntity.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+struct PlaceEntity: Decodable {
+    let id: String
+    let name: String
+    let latitude: Double
+    let longitude: Double
+    let images: [String]?
+    
+    func toDTO() -> Place {
+        return Place(id: id,
+                     name: name,
+                     coordinate: Coordinate(latitude: latitude, longitude: longitude),
+                     images: images)
+    }
+}
