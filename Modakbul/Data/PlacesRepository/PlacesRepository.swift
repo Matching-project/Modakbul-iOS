c1a9583397a1f1e544a44371a87c8efc5e44a3d6
YunSwain <destap@naver.com>
2024-06-19 16:58:10 +0900

    refactor: PlacesRepository 수정

--- Modakbul/Data/PlacesRepository/PlacesRepository.swift
+++ Modakbul/Data/PlacesRepository/PlacesRepository.swift
@@ -9,20 +9,20 @@ import Foundation
 import MapKit
 
 protocol MapControllable: AnyObject {
-    var region: MKCoordinateRegion { get set }
-    var suggestedResults: [String] { get set }
-    var searchingText: String { get set }
+    @MainActor func coordinateDidUpdate(_ locationService: LocationService, coordinate: Coordinate)
+    @MainActor func suggestedResultsDidUpdate(_ localMapService: LocalMapService, results: [String])
+    @MainActor func searchResultsDidUpdate(_ localMapService: LocalMapService, results: [Place])
 }
 
 protocol PlacesRepository: NSObject {
     var delegate: MapControllable? { get set }
     
     func requestLocationUpdate()
-    @MainActor func findPlaces(on region: MKCoordinateRegion, with keyword: String?) async throws -> [Place]
+    @MainActor func findPlaces(on region: MKCoordinateRegion, with keyword: String?) async throws
     func updateSearchingText(by text: String)
 }
 
-enum PlaceRepositoryError: Error {
+enum PlacesRepositoryError: Error {
     
 }
 
@@ -53,14 +53,19 @@ extension DefaultPlacesRepository: PlacesRepository {
         locationService.updateOnce()
     }
     
-    func findPlaces(on region: MKCoordinateRegion, with keyword: String?) async throws -> [Place] {
-        let locations = try await localMapService.findPlaces(on: region, with: keyword).map {
-            let coordinate = Coordinate(latitude: $0.placemark.coordinate.latitude, longitude: $0.placemark.coordinate.longitude)
-            return Location(name: $0.name, coordinate: coordinate)
+    @MainActor func findPlaces(on region: MKCoordinateRegion, with keyword: String?) async throws {
+        Task {
+            let locations = try await localMapService.findPlaces(on: region, with: keyword).map {
+                let coordinate = $0.placemark.coordinate.toDTO()
+                return Location(name: $0.name, coordinate: coordinate)
+            }
+//            let endpoint = Endpoint.fetchPlaces(locations: locations)
+//            let places = try await networkService.request(endpoint: endpoint, for: [PlaceEntity].self).map { $0.toDTO() }
+            delegate?.searchResultsDidUpdate(localMapService, results: [Place(id: "id", 
+                                                                              name: "name",
+                                                                              coordinate: Coordinate(latitude: 32.3141, longitude: 32.4345),
+                                                                              images: [])])
         }
-        
-        let endpoint = Endpoint.fetchPlaces(locations: locations)
-        return try await networkService.request(endpoint: endpoint, for: [PlaceEntity].self).map { $0.toDTO() }
     }
     
     func updateSearchingText(by text: String) {
@@ -70,18 +75,18 @@ extension DefaultPlacesRepository: PlacesRepository {
 
 // MARK: LocationServiceDelegate Confirmation
 extension DefaultPlacesRepository: LocationServiceDelegate {
-    func didUpdateCoordinate(coordinate: CLLocationCoordinate2D) {
-        delegate?.region.center = coordinate
+    @MainActor func didUpdateCoordinate(coordinate: CLLocationCoordinate2D) {
+        delegate?.coordinateDidUpdate(locationService, coordinate: coordinate.toDTO())
     }
     
     func didFailWithError(error: LocationServiceError) {
-        <#code#>
+        print(error)
     }
 }
 
 // MARK: LocalMapServiceDelegate Confirmation
 extension DefaultPlacesRepository: LocalMapServiceDelegate {
-    func suggestedResultsDidUpdate(localMapService: any LocalMapService, suggestedResults: [MKLocalSearchCompletion]) {
-        delegate?.suggestedResults = suggestedResults.map { $0.title }
+    @MainActor func suggestedResultsDidUpdate(localMapService: any LocalMapService, suggestedResults: [MKLocalSearchCompletion]) {
+        delegate?.suggestedResultsDidUpdate(localMapService, results: suggestedResults.map { $0.title })
     }
 }
