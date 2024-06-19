52479add8785492d0703d9cc1b3f5fefa199b9fe
YunSwain <destap@naver.com>
2024-06-19 16:57:04 +0900

    feat: 도메인 모델 변환 코드 확장

--- Modakbul/Domain/Models/Coordinate.swift
+++ Modakbul/Domain/Models/Coordinate.swift
@@ -6,8 +6,15 @@
 //
 
 import Foundation
+import CoreLocation
 
 struct Coordinate {
     let latitude: Double
     let longitude: Double
+}
+
+extension Coordinate {
+    func toCLCoordinate() -> CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
 }
