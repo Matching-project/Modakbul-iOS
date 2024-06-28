0b591fa0de51fb1eb2c9c3b1a333311f5328536f
shlim <jfknfj920@naver.com>
2024-06-28 18:47:47 +0900

    feat: User 관련 repository 준비

---
+++ Modakbul/Data/UserManagementRepository.swift
@@ -0,0 +1,24 @@
+//
+//  UserManagementRepository.swift
+//  Modakbul
+//
+//  Created by Lim Seonghyeon on 6/28/24.
+//
+
+// ValidateUserNicknameOverlappedUseCase
+// RegisterUserProfileUseCase
+// ReportUserUseCase
+// BlockUserUseCase
+// FetchBlockedUserUseCase
+// UnblockUserUseCase
+// UnregisterUserUseCase
+
+import Foundation
+
+protocol UserManagementRepository {
+    
+}
+
+final class DefaultUserManagementRepository: UserManagementRepository {
+    
+}
