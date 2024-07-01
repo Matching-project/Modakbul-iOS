2e1eeff0a0664f70bcbd4886f496b0b3c4d3cb48
shlim <jfknfj920@naver.com>
2024-07-01 23:39:06 +0900

    feat, refactor: NetworkSocketManager -> ChatService로 네이밍 변경

--- Modakbul/Application/DependencyContainer/Assembly/InfrastructureAssembly.swift
+++ Modakbul/Application/DependencyContainer/Assembly/InfrastructureAssembly.swift
@@ -11,10 +11,8 @@ struct InfrastructureAssembly: Assembly {
     func assemble(container: DependencyContainer) {
         // Network
         container.register(for: NetworkSessionManager.self, DefaultNetworkSessionManager())
-        container.register(for: NetworkSocketManager.self, DefaultNetworkSocketManager())
         container.register(for: NetworkService.self) { resolver in
-            DefaultNetworkService(sessionManager: resolver.resolve(NetworkSessionManager.self),
-                                  socketManager: resolver.resolve(NetworkSocketManager.self))
+            DefaultNetworkService(sessionManager: resolver.resolve(NetworkSessionManager.self))
         }
         
         // OAuth
@@ -26,6 +24,11 @@ struct InfrastructureAssembly: Assembly {
         // Location
         container.register(for: LocationService.self, DefaultLocationService())
         container.register(for: LocalMapService.self, DefaultLocalMapService())
+        
+        // Chat
+        container.register(for: ChatService.self) { resolver in
+            DefaultChatService(sessionManager: resolver.resolve(NetworkSessionManager.self))
+        }
         
         // Storages
         container.register(for: TokenStorage.self, DefaultTokenStorage())
