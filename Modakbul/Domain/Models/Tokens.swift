ac5fcf522e23c67c38f7f07c012f11fed1e53b6c
shlim <jfknfj920@naver.com>
2024-07-09 23:39:15 +0900

    feat: Added Tokens Type

---
+++ Modakbul/Domain/Models/Tokens.swift
@@ -0,0 +1,13 @@
+//
+//  Tokens.swift
+//  Modakbul
+//
+//  Created by Lim Seonghyeon on 7/9/24.
+//
+
+import Foundation
+
+struct Tokens: Codable {
+    let accessToken: String
+    let refreshToken: String
+}
