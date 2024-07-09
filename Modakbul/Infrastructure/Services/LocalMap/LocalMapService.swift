5b66fb5b1ff772dcb5b20a48bf5300f25277ca96
YunSwain <destap@naver.com>
2024-07-09 18:27:09 +0900

    refactor: 지도서비스 구현

--- Modakbul/Infrastructure/Services/LocalMap/LocalMapService.swift
+++ Modakbul/Infrastructure/Services/LocalMap/LocalMapService.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+import MapKit
+
+protocol LocalMapService: NSObject {
+    func search(by keyword: String, on coordinate: Coordinate) async throws -> [Location]
+}
+
+final class DefaultLocalMapService: NSObject {
+    private var span: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+}
+
+// MARK: LocalMapService Conformation
+extension DefaultLocalMapService: LocalMapService {
+    func search(by keyword: String, on coordinate: Coordinate) async throws -> [Location] {
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = keyword
+        request.region = MKCoordinateRegion(center: coordinate.toCLCoordinate(), span: span)
+        
+        let response = try await MKLocalSearch(request: request).start()
+        return response.mapItems.map { mapItem in
+            Location(mapItem.placemark)
+        }
+    }
+}
