3b5cbe628259396d8fbc190994c25cf204f6a666
YunSwain <destap@naver.com>
2024-07-09 18:28:34 +0900

    refactor: 카페제보화면 Route 추가

--- Modakbul/Presentation/NavigationRouteView/Route.swift
+++ Modakbul/Presentation/NavigationRouteView/Route.swift
@@ -8,7 +8,12 @@
 import SwiftUI
 
 protocol Routable: Hashable, Identifiable {
+    associatedtype Content: View
+    associatedtype Router: AppRouter
+    
     var presentingType: PresentingType { get }
+    
+    @ViewBuilder func view(with router: Router) -> Content
 }
 
 extension Routable {
@@ -22,15 +27,32 @@ enum PresentingType {
 }
 
 enum Route: Routable {
+    typealias Router = DefaultAppRouter
+    
     case loginView
     case homeView
     case myView
+    case placeShowcaseView
     
     var presentingType: PresentingType {
         switch self {
         case .loginView: return .fullScreenCover
         case .homeView: return .push
         case .myView: return .sheet(detent: .medium)
+        case .placeShowcaseView: return .push
+        }
+    }
+    
+    @ViewBuilder func view(with router: Router) -> some View {
+        switch self {
+        case .loginView:
+            LoginView<Router>(loginViewModel: router.resolver.resolve(LoginViewModel.self))
+        case .homeView:
+            HomeView<Router>(homeViewModel: router.resolver.resolve(HomeViewModel.self))
+        case .myView:
+            MyView<Router>()
+        case .placeShowcaseView:
+            PlaceShowcaseView<Router>(placeShowcaseViewModel: router.resolver.resolve(PlaceShowcaseViewModel.self))
         }
     }
 }
