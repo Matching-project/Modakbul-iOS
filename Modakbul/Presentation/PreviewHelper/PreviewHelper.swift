ea08396427fd1ffeccf7fecf9ffdf2a187872100
YunSwain <destap@naver.com>
2024-07-25 17:48:39 +0900

    refactor: 장소 목데이터 추가

--- Modakbul/Presentation/PreviewHelper/PreviewHelper.swift
+++ Modakbul/Presentation/PreviewHelper/PreviewHelper.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import CoreLocation
 
 extension PreviewProvider {
     static var previewHelper: PreviewHelper { PreviewHelper.shared }
@@ -21,6 +22,219 @@ final class PreviewHelper {
                                  PresentationAssembly())
     
     var resolver: DependencyResolver { router.resolver }
+    
+    var places: [Place] = [
+        Place(
+            id: UUID().uuidString,
+            location: Location(
+                name: "구로디지털단지",
+                address: "서울시 구로구 디지털로 26길 38",
+                coordinate: CLLocationCoordinate2D(latitude: 37.4848, longitude: 126.8963)
+            ),
+            openingHoursOfWeek: [.mon: Place.OpeningHours(
+                open: DateComponents(hour: 9, minute: 0),
+                close: DateComponents(hour: 18, minute: 0)
+            ), .tue: Place.OpeningHours(
+                open: DateComponents(hour: 9, minute: 0),
+                close: DateComponents(hour: 18, minute: 0)
+            ), .wed: Place.OpeningHours(
+                open: DateComponents(hour: 9, minute: 0),
+                close: DateComponents(hour: 18, minute: 0)
+            ), .thu: Place.OpeningHours(
+                open: DateComponents(hour: 9, minute: 0),
+                close: DateComponents(hour: 18, minute: 0)
+            ), .fri: Place.OpeningHours(
+                open: DateComponents(hour: 9, minute: 0),
+                close: DateComponents(hour: 18, minute: 0)
+            ), .sat: Place.OpeningHours(
+                open: DateComponents(hour: 9, minute: 0),
+                close: DateComponents(hour: 18, minute: 0)
+            ), .sun: Place.OpeningHours(
+                open: DateComponents(hour: 9, minute: 0),
+                close: DateComponents(hour: 18, minute: 0)
+            )],
+            powerSocketState: .plenty,
+            noiseLevel: .quiet,
+            groupSeatingState: .yes,
+            communities: [],
+            images: ["image1.png", "image2.png"]
+        ),
+        Place(
+            id: UUID().uuidString,
+            location: Location(
+                name: "구로시장",
+                address: "서울시 구로구 구로동 437-1",
+                coordinate: CLLocationCoordinate2D(latitude: 37.4953, longitude: 126.8882)
+            ),
+            openingHoursOfWeek: [.mon: Place.OpeningHours(
+                open: DateComponents(hour: 9, minute: 0),
+                close: DateComponents(hour: 18, minute: 0)
+            ), .tue: Place.OpeningHours(
+                open: DateComponents(hour: 9, minute: 0),
+                close: DateComponents(hour: 18, minute: 0)
+            ), .wed: Place.OpeningHours(
+                open: DateComponents(hour: 9, minute: 0),
+                close: DateComponents(hour: 18, minute: 0)
+            ), .thu: Place.OpeningHours(
+                open: DateComponents(hour: 9, minute: 0),
+                close: DateComponents(hour: 18, minute: 0)
+            ), .fri: Place.OpeningHours(
+                open: DateComponents(hour: 9, minute: 0),
+                close: DateComponents(hour: 18, minute: 0)
+            ), .sat: Place.OpeningHours(
+                open: DateComponents(hour: 9, minute: 0),
+                close: DateComponents(hour: 18, minute: 0)
+            ), .sun: Place.OpeningHours(
+                open: DateComponents(hour: 9, minute: 0),
+                close: DateComponents(hour: 18, minute: 0)
+            )],
+            powerSocketState: .moderate,
+            noiseLevel: .moderate,
+            groupSeatingState: .no,
+            communities: [],
+            images: nil
+        ),
+        Place(
+            id: UUID().uuidString,
+            location: Location(
+                name: "구로아트밸리",
+                address: "서울시 구로구 구로동 814",
+                coordinate: CLLocationCoordinate2D(latitude: 37.4958, longitude: 126.8874)
+            ),
+            openingHoursOfWeek: [.mon: Place.OpeningHours(
+                open: DateComponents(hour: 9, minute: 0),
+                close: DateComponents(hour: 18, minute: 0)
+            ), .tue: Place.OpeningHours(
+                open: DateComponents(hour: 9, minute: 0),
+                close: DateComponents(hour: 18, minute: 0)
+            ), .wed: Place.OpeningHours(
+                open: DateComponents(hour: 9, minute: 0),
+                close: DateComponents(hour: 18, minute: 0)
+            ), .thu: Place.OpeningHours(
+                open: DateComponents(hour: 9, minute: 0),
+                close: DateComponents(hour: 18, minute: 0)
+            ), .fri: Place.OpeningHours(
+                open: DateComponents(hour: 9, minute: 0),
+                close: DateComponents(hour: 18, minute: 0)
+            ), .sat: Place.OpeningHours(
+                open: DateComponents(hour: 9, minute: 0),
+                close: DateComponents(hour: 18, minute: 0)
+            ), .sun: Place.OpeningHours(
+                open: DateComponents(hour: 9, minute: 0),
+                close: DateComponents(hour: 18, minute: 0)
+            )],
+            powerSocketState: .few,
+            noiseLevel: .noisy,
+            groupSeatingState: .yes,
+            communities: [],
+            images: nil
+        ),
+        Place(
+            id: UUID().uuidString,
+            location: Location(
+                name: "가산디지털단지",
+                address: "서울시 금천구 가산동 371-28",
+                coordinate: CLLocationCoordinate2D(latitude: 37.4812, longitude: 126.8827)
+            ),
+            openingHoursOfWeek: [.mon: Place.OpeningHours(
+                open: DateComponents(hour: 9, minute: 0),
+                close: DateComponents(hour: 18, minute: 0)
+            ), .tue: Place.OpeningHours(
+                open: DateComponents(hour: 9, minute: 0),
+                close: DateComponents(hour: 18, minute: 0)
+            ), .wed: Place.OpeningHours(
+                open: DateComponents(hour: 9, minute: 0),
+                close: DateComponents(hour: 18, minute: 0)
+            ), .thu: Place.OpeningHours(
+                open: DateComponents(hour: 9, minute: 0),
+                close: DateComponents(hour: 18, minute: 0)
+            ), .fri: Place.OpeningHours(
+                open: DateComponents(hour: 9, minute: 0),
+                close: DateComponents(hour: 18, minute: 0)
+            ), .sat: Place.OpeningHours(
+                open: DateComponents(hour: 9, minute: 0),
+                close: DateComponents(hour: 18, minute: 0)
+            ), .sun: Place.OpeningHours(
+                open: DateComponents(hour: 9, minute: 0),
+                close: DateComponents(hour: 18, minute: 0)
+            )],
+            powerSocketState: .plenty,
+            noiseLevel: .quiet,
+            groupSeatingState: .no,
+            communities: [],
+            images: nil
+        ),
+        Place(
+            id: UUID().uuidString,
+            location: Location(
+                name: "디지털단지 지하상가",
+                address: "서울시 구로구 구로동 3-25",
+                coordinate: CLLocationCoordinate2D(latitude: 37.4845, longitude: 126.9013)
+            ),
+            openingHoursOfWeek: [.mon: Place.OpeningHours(
+                open: DateComponents(hour: 9, minute: 0),
+                close: DateComponents(hour: 18, minute: 0)
+            ), .tue: Place.OpeningHours(
+                open: DateComponents(hour: 9, minute: 0),
+                close: DateComponents(hour: 18, minute: 0)
+            ), .wed: Place.OpeningHours(
+                open: DateComponents(hour: 9, minute: 0),
+                close: DateComponents(hour: 18, minute: 0)
+            ), .thu: Place.OpeningHours(
+                open: DateComponents(hour: 9, minute: 0),
+                close: DateComponents(hour: 18, minute: 0)
+            ), .fri: Place.OpeningHours(
+                open: DateComponents(hour: 9, minute: 0),
+                close: DateComponents(hour: 18, minute: 0)
+            ), .sat: Place.OpeningHours(
+                open: DateComponents(hour: 9, minute: 0),
+                close: DateComponents(hour: 18, minute: 0)
+            ), .sun: Place.OpeningHours(
+                open: DateComponents(hour: 9, minute: 0),
+                close: DateComponents(hour: 18, minute: 0)
+            )],
+            powerSocketState: .moderate,
+            noiseLevel: .moderate,
+            groupSeatingState: .yes,
+            communities: [],
+            images: nil
+        ),
+        Place(
+            id: UUID().uuidString,
+            location: Location(
+                name: "구로중앙공원",
+                address: "서울시 구로구 구로동 685-101",
+                coordinate: CLLocationCoordinate2D(latitude: 37.4956, longitude: 126.8879)
+            ),
+            openingHoursOfWeek: [.mon: Place.OpeningHours(
+                open: DateComponents(hour: 9, minute: 0),
+                close: DateComponents(hour: 18, minute: 0)
+            ), .tue: Place.OpeningHours(
+                open: DateComponents(hour: 9, minute: 0),
+                close: DateComponents(hour: 18, minute: 0)
+            ), .wed: Place.OpeningHours(
+                open: DateComponents(hour: 9, minute: 0),
+                close: DateComponents(hour: 18, minute: 0)
+            ), .thu: Place.OpeningHours(
+                open: DateComponents(hour: 9, minute: 0),
+                close: DateComponents(hour: 18, minute: 0)
+            ), .fri: Place.OpeningHours(
+                open: DateComponents(hour: 9, minute: 0),
+                close: DateComponents(hour: 18, minute: 0)
+            ), .sat: Place.OpeningHours(
+                open: DateComponents(hour: 9, minute: 0),
+                close: DateComponents(hour: 18, minute: 0)
+            ), .sun: Place.OpeningHours(
+                open: DateComponents(hour: 9, minute: 0),
+                close: DateComponents(hour: 18, minute: 0)
+            )],
+            powerSocketState: .few,
+            noiseLevel: .noisy,
+            groupSeatingState: .no,
+            communities: [],
+            images: []
+        ),
+    ]
     
     private init() {}
 }
