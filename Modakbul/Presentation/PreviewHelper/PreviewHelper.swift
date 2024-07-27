dee48d23d25a674a838cc389afd00f3b87d4d15b
YunSwain <destap@naver.com>
2024-07-27 11:28:29 +0900

    refactor: 목데이터 추가

--- Modakbul/Presentation/PreviewHelper/PreviewHelper.swift
+++ Modakbul/Presentation/PreviewHelper/PreviewHelper.swift
@@ -56,7 +56,46 @@ final class PreviewHelper {
             powerSocketState: .plenty,
             noiseLevel: .quiet,
             groupSeatingState: .yes,
-            communities: [],
+            communities: [
+                CommunityReqruitingContent(
+                    id: UUID().uuidString,
+                    title: "개발 모각코 하실 분 구합니다!",
+                    content: "글의 내용입니다. 글의 내용입니다. 글의 내용입니다. 글의 내용입니다. 글의 내용입니다. ",
+                    writtenDate: .now,
+                    writer: User(
+                        name: "SwainYun",
+                        nickname: "SwainYun",
+                        email: "destap@naver.com",
+                        provider: .apple,
+                        gender: .male,
+                        job: .jobSeeker,
+                        categoriesOfInterest: [.coding, .design, .selfImprovement],
+                        isGenderVisible: true,
+                        birth: .now,
+                        imageURL: nil
+                    ),
+                    community: Community(
+                        routine: .daily,
+                        category: .coding,
+                        participants: [
+                            User(
+                                name: "SwainYun",
+                                nickname: "SwainYun",
+                                email: "destap@naver.com",
+                                provider: .apple,
+                                gender: .male,
+                                job: .jobSeeker,
+                                categoriesOfInterest: [.coding, .design, .selfImprovement],
+                                isGenderVisible: true,
+                                birth: .now,
+                                imageURL: nil
+                            )
+                        ],
+                        participantsLimit: 5,
+                        promiseDate: .init(date: .now, startTime: .now, endTime: .now)
+                    )
+                ),
+            ],
             images: ["image1.png", "image2.png"]
         ),
         Place(
