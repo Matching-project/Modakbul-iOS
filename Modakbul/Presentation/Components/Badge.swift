5984ceab35c1c0d918c4958e6c9e8f51cc4045f6
YunSwain <destap@naver.com>
2024-08-14 17:30:11 +0900

    refactor: DynamicType 적용 커스텀 뱃지 구현

---
+++ Modakbul/Presentation/Components/Badge.swift
@@ -0,0 +1,31 @@
+//
+//  Badge.swift
+//  Modakbul
+//
+//  Created by Swain Yun on 8/14/24.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    @ScaledMetric(relativeTo: .subheadline) private var padding = 4
+    
+    private let count: Int
+    
+    init(count: Int) {
+        self.count = count
+    }
+    
+    var body: some View {
+        Text("\(count)")
+            .font(.subheadline)
+            .padding(.horizontal, padding)
+            .foregroundStyle(.white)
+            .background {
+                if count > 0 {
+                    Capsule()
+                        .fill(.accent.gradient)
+                }
+            }
+    }
+}
