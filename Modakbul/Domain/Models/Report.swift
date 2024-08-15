01e0fddedbd746f8228545ae05c86e047b08ba69
shlim <jfknfj920@naver.com>
2024-08-15 14:08:00 +0900

    feat: 신고하기 뷰 / 뷰모델 추가

--- Modakbul/Domain/Models/Report.swift
+++ Modakbul/Domain/Models/Report.swift
@@ -7,25 +7,30 @@
 
 import Foundation
 
-enum ReportType {
+// TODO: - Selectable로 하는건?
+enum ReportType: CaseIterable, Hashable, CustomStringConvertible {
     case disparagement
     case pornography
     case commercialAdvertisement
     case fishing
     case religiousPropaganda
-    case other(description: String)
+    case other
     
     var description: String {
         switch self {
-        case .other(let description): return description
-        default: return String(describing: self)
+        case .disparagement: "욕설/비하"
+        case .pornography: "음란물/불건전한 만남 및 대화"
+        case .commercialAdvertisement: "상업적 광고 및 판매"
+        case .fishing: "유출/사칭/사기"
+        case .religiousPropaganda: "과도한 종교 활동"
+        case .other: "기타"
         }
     }
 }
 
 struct Report {
-    let type: ReportType
+    var type: ReportType
     let from: User
     let to: User
-    var description: String { type.description }
+    var description: String
 }
--- Modakbul/Presentation/Chat/ReportView.swift
+++ Modakbul/Presentation/Chat/ReportView.swift
@@ -7,9 +7,9 @@
 
 import SwiftUI
 
-// https://www.notion.so/8cdee4c8f8d549808e28381a025d6637?pvs=4
-// - 신고 사유를 여러 개 선택할 수 있나요? → 1가지.
 struct ReportView<Router: AppRouter>: View {
+    @FocusState private var isFocused: Bool
+    @Environment(\.colorScheme) private var colorScheme
     @EnvironmentObject private var router: Router
     @ObservedObject private var vm: ReportViewModel
     
@@ -18,25 +18,91 @@ struct ReportView<Router: AppRouter>: View {
     }
     
     var body: some View {
-        Text("fuck")
+        ScrollView {
+            VStack(spacing: 20) {
+                Spacer()
+                
+                ForEach(ReportType.allCases, id: \.self) { reportType in
+                    HStack {
+                        Image(systemName: vm.reportType == reportType ? "circle.inset.filled" : "circle")
+                        Text(reportType.description)
+                    }
+                    .foregroundStyle(vm.reportType == reportType ? .accent : (colorScheme == .dark ? .white : .black))
+                    .onTapGesture {
+                        vm.reportType = reportType
+                    }
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                
+                RoundedTextField(
+                    text: $vm.description,
+                    axis: .vertical,
+                    lineLimit: 15,      // TODO: - 기종별 대응 필요
+                    disabled: vm.reportType == .other ? false : true,
+                    color: vm.reportType == .other ? .accent : (colorScheme == .dark ? .white : .black)
+                )
+                .focused($isFocused)
+                .padding(.leading, 27)
+                .padding(.trailing, 1)
+            }
+            .padding(.horizontal, 30)
+        }
+        .contentShape(Rectangle())
+        .onTapGesture {
+            isFocused = false
+        }
+        .navigationModifier(title: "신고하기") {
+            vm.initialize()
+            router.dismiss()
+        }
+        
+        FlatButton("신고완료") {
+            vm.showAlert = true
+        }
+        .alert("신고 완료", isPresented: $vm.showAlert) {
+            Button(role: .cancel) {
+                vm.submit()
+                vm.initialize()
+                router.dismiss()
+            } label: {
+                Text("확인")
+            }
+        } message: {
+            Text("관리자가 검토하기까지 2~3일 정도 소요됩니다. 마이페이지 - 나의 신고내역에서 상태를 확인할 수 있어요.")
+        }
+        .disabled(vm.reportType == nil ? true : false)
+        .padding(.horizontal, 30)
+        .padding(.vertical, 5)
     }
 }
 
 struct ReportView_Preview: PreviewProvider {
     static var previews: some View {
-        NavigationStack {
+//        NavigationStack {
             router.view(to: .reportView)
-                .navigationBarBackButtonHidden()
-                .navigationBarItems(leading: BackButton {
-                    
-                })
-                // TODO: - 최초 게시 여부에 따라 모집글 작성 / 모집글 수정으로 분기 필요
-                .navigationTitle("-----------------")
-                .navigationBarTitleDisplayMode(.inline)
-        }
+//                .navigationModifier(title: "신고하기") {
+//                    router.dismiss()
+//                }
+//        }
     }
 }
 
 final class ReportViewModel: ObservableObject {
+    @Published var reportType: ReportType? = nil
+    @Published var description: String = ""
+    @Published var showAlert: Bool = false
     
+    func initialize() {
+        reportType = nil
+        description = ""
+    }
+    
+    func submit() {
+        if reportType != .other {
+            description = ""
+        }
+        
+        // TODO: - UseCase 연결 필요
+        //        Report(type: reportType, from: <#T##User#>, to: <#T##User#>, description: description)
+    }
 }
