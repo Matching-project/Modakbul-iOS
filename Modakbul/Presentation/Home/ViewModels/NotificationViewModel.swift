58d93e3e046e9bbd9e746bca60f980a81b01fe2f
shlim <jfknfj920@naver.com>
2024-08-16 03:21:10 +0900

    feat, refactor: 알림 뷰 / 뷰모델 리팩터링. 알림 편집 및 삭제(선택삭제, 전체삭제, 스와이프삭제) 기능 추가

---
+++ Modakbul/Presentation/Home/ViewModels/NotificationViewModel.swift
@@ -0,0 +1,44 @@
+//
+//  NotificationViewModel.swift
+//  Modakbul
+//
+//  Created by Lim Seonghyeon on 8/16/24.
+//
+
+import SwiftUI
+
+final class NotificationViewModel: ObservableObject {
+    // TODO: - UseCase 연결 필요
+    //    private let notificationUseCase: NotificationUseCase
+    
+    @Published var notifications: [PushNotification] = PreviewHelper.shared.notifications
+    @Published var multiSelection = Set<UUID>()
+    @Environment(\.editMode) var editMode
+
+    var isEditingEnabled: Bool {
+        editMode?.wrappedValue.isEditing == true && !notifications.isEmpty
+    }
+    
+    func deleteNotification(_ notification: PushNotification) {
+        if let index = notifications.firstIndex(where: { $0.id == notification.id }) {
+            notifications.remove(at: index)
+        }
+    }
+    
+    func deleteSelectedNotifications() {
+        notifications.removeAll { notification in
+            multiSelection.contains(notification.id)
+        }
+        
+        multiSelection.removeAll()
+    }
+    
+    func deleteAllNotifications() {
+       notifications.removeAll()
+       toggleEditMode()
+   }
+    
+    func toggleEditMode() {
+        editMode?.wrappedValue = editMode?.wrappedValue.isEditing == true ? .inactive : .active
+    }
+}
