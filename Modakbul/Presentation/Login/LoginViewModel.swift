d0bd73220ccd8f5a7935067d991d625c39e8613c
YunSwain <destap@naver.com>
2024-08-18 14:12:27 +0900

    refactor: 데이터 타입 수정

--- Modakbul/Presentation/Login/LoginViewModel.swift
+++ Modakbul/Presentation/Login/LoginViewModel.swift
@@ -21,16 +21,13 @@ final class LoginViewModel: ObservableObject {
     // 카카오로그인은 서버로 토큰 줄 필요 없고,
     // 애플로그인일 때만 서버로 토큰 보내야 하지 않나?
     
-    func loginWithKakaoTalk(_ kakaoUser: KakaoUser) {
+    func loginWithKakaoTalk(_ token: OAuthToken) {
         Task {
             guard let token = try? JSONEncoder().encode(token) else {
                 return print("카카오 로그인 실패")
             }
-            guard let id = kakaoUser.id else {
-                return print("카카오 CI 존재하지 않음")
-            }
             
-            let credential = UserCredential(id: String(id), email: email, provider: .kakao)
+            let credential = UserCredential(authorizationCode: token, provider: .kakao)
             // TODO: 결과값 처리
             _ = await userRegistrationUseCase.login(token, by: .kakao)
         }
@@ -44,26 +41,7 @@ final class LoginViewModel: ObservableObject {
             }
             
             // TODO: 결과값 처리
-            if let familyName = appleIDCredential.fullName?.familyName,
-               let givenName = appleIDCredential.fullName?.givenName,
-               let email = appleIDCredential.email {
-                // MARK: - 최초 로그인시
-                let credential = UserCredential(id: appleIDCredential.user,
-                                                familyName: familyName,
-                                                givenName: givenName,
-                                                email: email,
-                                                authorizationCode: authorizationCode,
-                                                provider: .apple)
-                
-                _ = await userRegistrationUseCase.login(credential)
-            } else {
-                // MARK: - 재로그인시
-                let credential = UserCredential(id: appleIDCredential.user,
-                                                authorizationCode: authorizationCode,
-                                                provider: .apple)
-                
-                _ = await userRegistrationUseCase.login(credential)
-            }
+            _ = await userRegistrationUseCase.login(authorizationCode, by: .apple)
         }
     }
     
