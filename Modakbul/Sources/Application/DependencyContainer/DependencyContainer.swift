bb384dcd341647617d4151af60b503f922b550ed
YunSwain <destap@naver.com>
2024-08-26 17:00:50 +0900

    refactor: 의존성 등록 간 즉시 캐싱

--- Modakbul/Sources/Application/DependencyContainer/DependencyContainer.swift
+++ Modakbul/Sources/Application/DependencyContainer/DependencyContainer.swift
@@ -28,12 +28,6 @@ final class DefaultDependencyContainer {
             return instance
         }
         
-        if let closure = dependencies[key] as? (DependencyResolver) -> T {
-            let instance = closure(self)
-            dependencies[key] = instance
-            return instance
-        }
-        
         fatalError("의존성 객체를 찾을 수 없음: \(key)")
     }
 }
@@ -45,7 +39,8 @@ extension DefaultDependencyContainer: DependencyContainer {
     }
     
     func register<T>(for type: T.Type, _ handler: @escaping (any DependencyResolver) -> T) {
-        _register(for: type, handler)
+        let instance = handler(self)
+        _register(for: type, instance)
     }
     
     func resolve<T>(_ type: T.Type) -> T {
