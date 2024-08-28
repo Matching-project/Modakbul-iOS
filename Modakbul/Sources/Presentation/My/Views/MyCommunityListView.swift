71fe02b97919f498aaf9f47b29754de11ecb793c
YunSwain <destap@naver.com>
2024-08-28 15:18:17 +0900

    feat: 참여 요청 내역 화면 구현

---
+++ Modakbul/Sources/Presentation/My/Views/MyCommunityListView.swift
@@ -0,0 +1,40 @@
+//
+//  MyCommunityListView.swift
+//  Modakbul
+//
+//  Created by Swain Yun on 8/28/24.
+//
+
+import SwiftUI
+
+struct MyCommunityListView<Router: AppRouter>: View {
+    @EnvironmentObject private var router: Router
+    @ObservedObject private var viewModel: MyCommunityListViewModel
+    
+    init(_ viewModel: MyCommunityListViewModel) {
+        self.viewModel = viewModel
+    }
+    
+    var body: some View {
+        VStack {
+            SelectionTab(
+                selectedTab: $viewModel.selectedTab,
+                viewModel.selection,
+                viewModel.communityRecruitingContents) { content in
+                    content.activeState == viewModel.selectedTab
+                } onSelectCell: { content in
+                    router.route(to: .placeInformationDetailView(communityRecruitingContentId: content.id))
+                }
+        }
+        .padding()
+        .navigationTitle("참여 모임 내역")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct MyCommunityListView_Preview: PreviewProvider {
+    static var previews: some View {
+        MyCommunityListView<DefaultAppRouter>(MyCommunityListViewModel())
+            .environmentObject(router)
+    }
+}
