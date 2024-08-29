9877349ec56e9ac931aa873dbedf43a4dda4f1f7
YunSwain <destap@naver.com>
2024-08-29 13:46:57 +0900

    feat: StrokedFilledButton 구현

--- Modakbul/Sources/Presentation/Components/StrokedButton.swift
+++ Modakbul/Sources/Presentation/Components/StrokedButton.swift
@@ -45,3 +45,43 @@ struct StrokedButton<Content: View, ClipShape: Shape>: View {
         }
     }
 }
+
+struct StrokedFilledButton<Content: View, ClipShape: Shape>: View {
+    private let clipShape: ClipShape
+    private let edge: Edge.Set
+    private let length: CGFloat
+    private let content: () -> Content
+    private let action: () -> Void
+    
+    init(
+        _ shape: ClipShape,
+        _ padding: Edge.Set = .all,
+        _ length: CGFloat = 10,
+        content: @escaping () -> Content,
+        action: @escaping () -> Void
+    ) {
+        self.clipShape = shape
+        self.edge = padding
+        self.length = length
+        self.content = content
+        self.action = action
+    }
+    
+    var body: some View {
+        Button {
+            action()
+        } label: {
+            content()
+                .font(.headline)
+                .padding(edge, length)
+                .foregroundStyle(.white)
+                .background(.accent)
+                .overlay(
+                    clipShape
+                        .stroke(.white, lineWidth: 4)
+                )
+                .clipShape(clipShape)
+                .shadow(color: .secondary, radius: 4, y: 4)
+        }
+    }
+}
