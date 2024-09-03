c16c6d21a334017e74cecd43933855e25f2cf7be
shlim <jfknfj920@naver.com>
2024-09-03 23:52:26 +0900

    fix: APNs 토큰 갱신에 따른 FCM 토큰 갱신 이슈 해결

--- Modakbul/Sources/Application/AppDelegate.swift
+++ Modakbul/Sources/Application/AppDelegate.swift
@@ -9,7 +9,7 @@ import UIKit
 import FirebaseCore
 import FirebaseMessaging
 
-// MARK: - Firebase Quickstart Samples for iOS by Google: https://github.com/firebase/quickstart-ios/blob/14c812998f4fea0338a09bfec877470a1358ff80/messaging/MessagingExampleSwift/AppDelegate.swift#L116-L159
+// MARK: - Firebase Quickstart Samples for iOS by Google: https://github.com/firebase/quickstart-ios/blob/14c812998f4fea0338a09bfec877470a1358ff80/messaging/MessagingExamㅈpleSwift/AppDelegate.swift#L116-L159
 final class AppDelegate: UIResponder, UIApplicationDelegate {
     // TODO: - 메세지 식별시 사용 예정 (푸시 알림 타입에 따라 화면 분기 필요)
     //    let gcmMessageIDKey = "gcm.message_id"
@@ -20,7 +20,6 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         
         FirebaseApp.configure()
         Messaging.messaging().delegate = self
-        Messaging.messaging().isAutoInitEnabled = true
         UNUserNotificationCenter.current().delegate = self
         
         Task { @MainActor in
@@ -41,6 +40,12 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
                      didFailToRegisterForRemoteNotificationsWithError error: Error) {
         print("Unable to register for remote notifications: \(error.localizedDescription)")
     }
+    
+    // MARK: - APNs 토큰 갱신 및 FCM 토큰 갱신
+    func application(_ application: UIApplication,
+                     didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Messaging.messaging().apnsToken = deviceToken
+    }
 }
 
 extension AppDelegate: UNUserNotificationCenterDelegate {
@@ -53,14 +58,14 @@ extension AppDelegate: UNUserNotificationCenterDelegate {
         //        if let messageID = userInfo[gcmMessageIDKey] {
         //            print("Message ID: \(messageID)")
         //        }
-    
+        
         if let aps = userInfo["aps"] as? [String: Any],
            let alert = aps["alert"] as? [String: Any] {
             let title = alert["title"] as? String ?? "제목 없음"
             let body = alert["body"] as? String ?? "내용 없음"
             
-//            print("🔴 Recevied PushNotification from Foreground")
-//            print(userInfo)
+            print("🔴 Recevied PushNotification from Foreground")
+            print(userInfo)
             
             // TODO: - 백엔드에 의해 imageURL, timestamp, type 수정 예정
             NotificationManager.shared.notifications.append(PushNotification(imageURL: PreviewHelper.url1, title: title, subtitle: body, timestamp: "방금", type: .request))
@@ -81,8 +86,8 @@ extension AppDelegate: UNUserNotificationCenterDelegate {
         //            print("Message ID: \(messageID)")
         //        }
         
-//        print("🔴 Touched PushNotification")
-//        print(userInfo)
+        print("🔴 Touched PushNotification")
+        print(userInfo)
         
         NotificationManager.shared.lastNotification?.isTouched = true
     }
@@ -91,7 +96,13 @@ extension AppDelegate: UNUserNotificationCenterDelegate {
 // MARK: - FCM 토큰 수신
 extension AppDelegate: MessagingDelegate {
     func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
-        print("Firebase registration token: \(String(describing: fcmToken))")
+        Messaging.messaging().token { token, error in
+            if let error = error {
+                print("Error fetching FCM registration token: \(error)")
+            } else if let token = token {
+                print("🔴 FCM registration token: \(token)")
+            }
+        }
         
         let dataDict: [String: String] = ["token": fcmToken ?? ""]
         NotificationCenter.default.post(
