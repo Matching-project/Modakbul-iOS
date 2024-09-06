f17cf6aa7be7fd44280d568ca2196763790d6b5e
YunSwain <destap@naver.com>
2024-09-06 18:17:43 +0900

    feat: AppStorage 키 하드코딩 방지

---
+++ Modakbul/Sources/Domain/Models/AppStorageKey.swift
@@ -0,0 +1,16 @@
+//
+//  AppStorageKey.swift
+//  Modakbul
+//
+//  Created by Swain Yun on 9/6/24.
+//
+
+import Foundation
+
+/*
+ AppStorage의 키로 사용되는 항목들 입니다.
+ */
+struct AppStorageKey {
+    static let isFirstLaunch: String = "isFirstLaunch"
+    static let userId: String = "userId"
+}
