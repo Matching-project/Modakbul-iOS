2463b8e17325109d05169aff887cbd4222dcc8bb
YunSwain <destap@naver.com>
2024-09-07 16:30:32 +0900

    refactor: NotificationUseCase 정의

--- Modakbul/Sources/Domain/UseCases/Notification/NotificationUseCase.swift
+++ Modakbul/Sources/Domain/UseCases/Notification/NotificationUseCase.swift
@@ -12,24 +12,19 @@ protocol NotificationUseCase {
     /// - Parameters:
     ///   - notificiation: 보낼 알림을 의미합니다.
     ///   - userId: 보낼 상대방의 아이디를 의미합니다.
-    func send(_ notificiation: PushNotification, to userId: Int64) async throws
+    func send(_ notificiation: PushNotification, from userId: Int64, to opponentUserId: Int64) async throws
     
     /// 알림 목록을 조회합니다.
     /// - Returns: 1개 이상의 알림 배열입니다.
-    func fetch() async throws -> [PushNotification]
-
-    /// 사용자가 읽지 않은 알림 개수를 조회합니다.
-    /// - Returns: 1개 이상의 안 읽은 알림 개수입니다.
-    func fetchUnreadCount() async throws -> Int
+    func fetch(userId: Int64) async throws -> [PushNotification]
 
     /// 1개 이상의 알림을 삭제합니다.
     /// - Parameter ids: 삭제할 알림의 아이디입니다.
-    func remove(_ notificatioIds: [Int]) async throws
+    func remove(userId: Int64, _ notificatioIds: Int64...) async throws
     
     /// 알림을 읽음처리 합니다.
     /// - Parameter id: 읽음 처리할 알림 아이디입니다.
-    func read(_ notificatioIds: Int) async throws
-    
+    func read(userId: Int64, _ notificatioIds: Int64) async throws
 }
 
 final class DefaultNotificationUseCase {
