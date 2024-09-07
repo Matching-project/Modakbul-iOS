0dcdbb76673f613e9c715374dfbbe64769fefc35
shlim <jfknfj920@naver.com>
2024-09-07 16:50:06 +0900

    chore: 오타 수정

--- Modakbul/Sources/Domain/UseCases/Notification/NotificationUseCase.swift
+++ Modakbul/Sources/Domain/UseCases/Notification/NotificationUseCase.swift
@@ -11,20 +11,24 @@ protocol NotificationUseCase {
     /// 알림을 전송합니다.
     /// - Parameters:
     ///   - notificiation: 보낼 알림을 의미합니다.
-    ///   - userId: 보낼 상대방의 아이디를 의미합니다.
+    ///   - userId: 나의 아이디를 의미합니다.
+    ///   - opponentUserId: 상대방의 아이디를 의미합니다.
     func send(_ notificiation: PushNotification, from userId: Int64, to opponentUserId: Int64) async throws
     
     /// 알림 목록을 조회합니다.
+    /// - Parameter userId: 나의 아이디를 의미합니다.
     /// - Returns: 1개 이상의 알림 배열입니다.
     func fetch(userId: Int64) async throws -> [PushNotification]
 
     /// 1개 이상의 알림을 삭제합니다.
-    /// - Parameter ids: 삭제할 알림의 아이디입니다.
-    func remove(userId: Int64, _ notificatioIds: Int64...) async throws
+    /// - Parameter userId: 나의 아이디를 의미합니다.
+    /// - Parameter notificationIds: 삭제할 알림의 아이디입니다.
+    func remove(userId: Int64, _ notificationIds: Int64...) async throws
     
     /// 알림을 읽음처리 합니다.
-    /// - Parameter id: 읽음 처리할 알림 아이디입니다.
-    func read(userId: Int64, _ notificatioIds: Int64) async throws
+    /// - Parameter userId: 나의 아이디를 의미합니다.
+    /// - Parameter notificationIds: 읽음 처리할 알림 아이디입니다.
+    func read(userId: Int64, _ notificationIds: Int64) async throws
 }
 
 final class DefaultNotificationUseCase {
@@ -33,23 +37,19 @@ final class DefaultNotificationUseCase {
 }
 
 extension DefaultNotificationUseCase: NotificationUseCase {
-    func send(_ notificiation: PushNotification, to userId: Int64) async throws {
-        
+    func send(_ notificiation: PushNotification, from userId: Int64, to opponentUserId: Int64) async throws {
+        <#code#>
     }
     
-    func fetch() async throws -> [PushNotification] {
-        
+    func fetch(userId: Int64) async throws -> [PushNotification] {
+        <#code#>
     }
     
-    func fetchUnreadCount() async throws -> Int {
-
+    func remove(userId: Int64, _ notificationIds: Int64...) async throws {
+        <#code#>
     }
     
-    func remove(_ notificationIds: [Int]) async throws {
-        
-    }
-    
-    func read(_ notificationIds: Int) async throws {
-        
+    func read(userId: Int64, _ notificationIds: Int64) async throws {
+        <#code#>
     }
 }
