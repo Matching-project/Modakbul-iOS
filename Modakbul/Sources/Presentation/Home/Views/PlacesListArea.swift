c1b0caa0032c5481f0015f8402c4e166be1f1cda
YunSwain <destap@naver.com>
2024-09-21 17:54:30 +0900

    refactor: 장소목록화면 내 각 셀이 같은 뷰모델을 공유하여 발생하는 문제 해결을 위해 화면 분리

--- Modakbul/Sources/Presentation/Home/Views/PlacesListArea.swift
+++ Modakbul/Sources/Presentation/Home/Views/PlacesListArea.swift
@@ -59,7 +59,7 @@ struct PlacesListArea<Router: AppRouter>: View {
     
     private var listArea: some View {
         List(viewModel.places, id: \.id) { place in
-            router.view(to: .placeInformationView(place: place, displayMode: .summary))
+            Cell(place)
                 .listRowSeparator(.hidden)
         }
         .listStyle(.plain)
@@ -103,5 +103,125 @@ struct PlacesListArea<Router: AppRouter>: View {
             }
         }
         .padding()
+    }
+}
+
+extension PlacesListArea {
+    struct Cell: View {
+        @EnvironmentObject private var router: Router
+        @Environment(\.colorScheme) private var colorScheme
+        @State private var selectedOpeningHourOfDay: OpeningHour? {
+            didSet { displaySelectedOpeningHourText() }
+        }
+        @State private var openingHourText: String = String()
+        
+        private let place: Place
+        
+        init(_ place: Place) {
+            self.place = place
+        }
+        
+        var body: some View {
+            VStack {
+                HStack {
+                    if let url = place.imageURLs.first {
+                        AsyncImageView(url: url, minWidth: 100, minHeight: 100)
+                    } else {
+                        Image(colorScheme == .light ? .modakbulMainLight : .modakbulMainDark)
+                            .resizable()
+                            .aspectRatio(1, contentMode: .fill)
+                            .frame(width: 100, height: 100)
+                    }
+                    
+                    informationArea
+                    
+                    Spacer()
+                }
+                .overlay(alignment: .topTrailing) {
+                    communityRecruitingContentEditButton
+                }
+                
+                Spacer()
+                
+                HStack(spacing: 20) {
+                    Image(.marker)
+                    
+                    Text("모임 \(place.meetingCount)개 진행 중")
+                }
+                
+                Spacer()
+            }
+        }
+        
+        private var informationArea: some View {
+            VStack(alignment: .leading) {
+                VStack(alignment: .leading, spacing: 4) {
+                    Text(place.location.name)
+                        .font(.Modakbul.title3)
+                        .bold()
+                    
+                    Text(place.location.address)
+                        .font(.Modakbul.caption)
+                }
+                .padding(.top)
+                
+                HStack {
+                    Text("운영시간 ")
+                    
+                    Menu {
+                        ForEach(place.openingHours, id: \.dayOfWeek) { openingHour in
+                            Button {
+                                selectedOpeningHourOfDay = openingHour
+                            } label: {
+                                Text(displayOpeningHours(openingHour))
+                            }
+                        }
+                    } label: {
+                        Text(openingHourText)
+                        Image(systemName: "chevron.down")
+                    }
+                }
+                
+                HStack {
+                    CapsuleTag(place.powerSocketState.description, .Modakbul.caption)
+                    CapsuleTag(place.groupSeatingState.description, .Modakbul.caption)
+                }
+            }
+            .font(.Modakbul.caption)
+        }
+        
+        private var communityRecruitingContentEditButton: some View {
+            Button {
+                router.route(to: .placeInformationDetailMakingView(place: place, communityRecruitingContent: nil))
+            } label: {
+                Image(.photoUploadSelection)
+                    .resizable()
+                    .scaledToFit()
+            }
+            .frame(width: 30, height: 30)
+            // MARK: - https://stackoverflow.com/q/56561064
+            .buttonStyle(BorderlessButtonStyle())
+            .shadow(color: .gray.opacity(0.3), radius: 4, y: 4)
+        }
+        
+        private func configureView(by place: Place) {
+            let calendar = Calendar.current
+            let weekDay = calendar.component(.weekday, from: .now)
+            let dayOfWeek = DayOfWeek(weekDay)
+            selectedOpeningHourOfDay = place.openingHours.first(where: {$0.dayOfWeek == dayOfWeek})
+        }
+        
+        private func displayOpeningHours(_ openingHour: OpeningHour) -> String {
+            let dayOfWeek = openingHour.dayOfWeek.description
+            let open = openingHour.open
+            let close = openingHour.close
+            return "\(dayOfWeek) \(open) - \(close)"
+        }
+        
+        private func displaySelectedOpeningHourText() {
+            if let openingHour = selectedOpeningHourOfDay {
+                openingHourText = displayOpeningHours(openingHour)
+            }
+        }
     }
 }
