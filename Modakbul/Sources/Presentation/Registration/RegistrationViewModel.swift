1027d37644abc34bf9a76e8156e13cf3c5be7aef
shlim <jfknfj920@naver.com>
2024-10-09 17:16:36 +0900

    fix: 회원가입 로직 재진입시 라우팅 에러 수정

--- Modakbul/Sources/Presentation/Registration/RegistrationViewModel.swift
+++ Modakbul/Sources/Presentation/Registration/RegistrationViewModel.swift
@@ -136,6 +136,7 @@ final class RegistrationViewModel: ObservableObject {
         categoriesOfInterest = []
         image = nil
         currentField = .name
+        fieldIndex = 0
         integrityResult = nil
         isWaiting = false
     }
