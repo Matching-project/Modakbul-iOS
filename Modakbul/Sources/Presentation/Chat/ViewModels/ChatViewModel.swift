65d8e8127c4aecb70f72d8619bef0035638592a2
shlim <jfknfj920@naver.com>
2024-10-12 16:47:19 +0900

    fix: 임시 테스트 값 삭제

--- Modakbul/Sources/Presentation/Chat/ViewModels/ChatViewModel.swift
+++ Modakbul/Sources/Presentation/Chat/ViewModels/ChatViewModel.swift
@@ -163,7 +163,7 @@ extension ChatViewModel {
         let chatMessage = ChatMessage(
             chatRoomId: chatRoomId,
             senderId: userId,
-            senderNickname: "Jjong",
+            senderNickname: userNickname,
             content: textOnTextField,
             sendTime: .now,
             unreadCount: 1
