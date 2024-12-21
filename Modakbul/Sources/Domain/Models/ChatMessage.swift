d4024b9c72e8aa2593a79a806fb218052d25616d
shlim <jfknfj920@naver.com>
2024-12-22 00:45:11 +0900

    fix: 본인이 안 읽은 메시지 수 만큼 카운트 되지 않는 로직 수정
    다만, 채팅방에 진입전까지는 채팅목록 화면에서 최신화 된 채팅 메세지를 확인할 수 없는 구조이므로, 실질적으로는 채팅목록 화면에서의 Badge는 항상 0에 수렴하게 되니 유의

--- Modakbul/Sources/Domain/Models/ChatMessage.swift
+++ Modakbul/Sources/Domain/Models/ChatMessage.swift
@@ -21,7 +21,8 @@ final class ChatRoom: Identifiable {
     private var _unreadMessagesCount: Int = 0
     var unreadMessagesCount: Int {
         get {
-            let count = messages.filter { $0.isRead == false && $0.senderId != Constants.loggedOutUserId && $0.senderId == opponentUserId }.count
+            // MARK: - 기존 $0.isRead == false 조건에서 $0.unreadCount != 0로 수정함
+            let count = messages.filter { $0.unreadCount != 0 && $0.senderId != Constants.loggedOutUserId && $0.senderId == opponentUserId }.count
             return max(_unreadMessagesCount, count)
         }
         
