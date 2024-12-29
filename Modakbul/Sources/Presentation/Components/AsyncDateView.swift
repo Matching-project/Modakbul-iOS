7f16fcb0b8e32d38519e2501f1bf43ba94e4c597
YunSwain <destap@naver.com>
2024-12-29 11:09:21 +0900

    refactor: DatePicker를 통해 selection이 바뀌어 날짜 및 시간 표시 가능하도록 개선

--- Modakbul/Sources/Presentation/Components/AsyncDateView.swift
+++ Modakbul/Sources/Presentation/Components/AsyncDateView.swift
@@ -9,24 +9,37 @@ import SwiftUI
 
 struct AsyncDateView: View {
     @State private var time: String = String()
+    @Binding var date: Date
     
-    let date: Date
     let format: DateFormat
     let font: Font?
+    
+    init(
+        date: Binding<Date>,
+        format: DateFormat,
+        font: Font? = nil
+    ) {
+        self._date = date
+        self.format = format
+        self.font = font
+    }
     
     init(
         date: Date,
         format: DateFormat,
         font: Font? = nil
     ) {
-        self.date = date
-        self.format = format
-        self.font = font
+        self.init(date: .constant(date), format: format, font: font)
     }
     
     var body: some View {
         Text(time)
             .font(font)
+            .onChange(of: date) { _, newValue in
+                Task { @MainActor in
+                    time = await newValue.toString(by: format)
+                }
+            }
             .task {
                 time = await date.toString(by: format)
             }
